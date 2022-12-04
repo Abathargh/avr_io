b6adcd668326e31ca42a5961459d6b0d17da0a90
mar <gim.marcello@gmail.com>
2022-12-04 19:06:39 +0100

    added: full IO register mappings for the atmega644.

--- src/avr_io/private/mapped_io.nim
+++ src/avr_io/private/mapped_io.nim
@@ -1,10 +1,23 @@
 import volatile
 
-type MappedIoRegister* = distinct ptr uint16
 
-template `[]`*(p: MappedIoRegister): uint16 =
-  volatile.volatileLoad((ptr uint16)p)
+type MappedIoRegister8* = distinct ptr uint8
+type MappedIoRegister16* = distinct ptr uint
 
-template `[]=`*(p: MappedIoRegister, v: uint16) =
+template `[]`*(p: MappedIoRegister8): uint8 =
+  volatile.volatileLoad((ptr uint8)p)
+
+template `[]=`*(p: MappedIoRegister8, v: uint8) =
   volatile.volatileStore((ptr uint16)p, v)
 
+template `[]`*(p: MappedIoRegister16): uint16 =
+  volatile.volatileLoad((ptr uint16)p)
+
+template `[]=`*(p: MappedIoRegister16, v: uint16) =
+  volatile.volatileStore((ptr uint16)p, v)
+
+template ioPtr8(a: uint8): ptr uint8 = 
+  cast[ptr uint8](a)
+
+template ioPtr16(a: uint16): ptr uint16 = 
+  cast[ptr uint16](a)
