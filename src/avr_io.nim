bfb8cf136c3ea0b1177be835782f55e59d272791
mar <gim.marcello@gmail.com>
2022-12-09 15:02:27 +0100

    changed: isr can now be implemented through the 'isr' macro/macro pragma; added compile time errors when an architecture/part number is not specified or is not found.

--- src/avr_io.nim
+++ src/avr_io.nim
@@ -1,4 +1,7 @@
+import macros 
+
 when defined(USING_ATMEGA644):
   include avr_io/private/atmega644
 else:
-  echo "undefined architecture"
+  static:
+    error "undefined architecture"
--- src/avr_io/interrupt.nim
+++ src/avr_io/interrupt.nim
@@ -1,14 +1,26 @@
 # TODO: IST attributes (block, non block, naked, aliasof)
 
+import macros
+
 when defined(USING_ATMEGA644):
-  include avr_io/interrupt/private/atmega644
+  include interrupt/private/atmega644
 else:
-  echo "undefined architecture"
+  static:
+    error "undefined architecture"
 
 
-template vectorDecl*(interrupt: VectorInterrupt): string =
-  const n = $ord(interrupt)
-  "$1  __vector_" & n & "$3 __attribute__((__signal__,__used__,__externally_visible__)); $1 __vector_" & n & "$3"
+template vectorDecl(n: int): string =
+  "$1  __vector_" & $n & "$3 __attribute__((__signal__,__used__,__externally_visible__)); $1 __vector_" & $n & "$3"
+
+
+macro isr*(v: static[VectorInterrupt], p: untyped): untyped =
+  var pnode = p
+  if p.kind == nnkStmtList:
+    pnode = p[0]
+  expectKind(pnode, nnkProcDef)
+  addPragma(pnode, newIdentNode("exportc"))
+  addPragma(pnode, newNimNode(nnkExprColonExpr).add(newIdentNode("codegenDecl"), newLit(vectorDecl(ord(v)))))
+  pnode
 
 proc sei*() =
   asm """
