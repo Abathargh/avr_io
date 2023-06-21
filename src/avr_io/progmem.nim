44f0444396f72197f00bb138928660f6d8597601
mar <gim.marcello@gmail.com>
2023-06-21 23:58:34 +0200

    added: wrappers for the exposed progmem functions in avr/pgmspace.h. changed: ProgramMemory as generic type instead of a specific type for arrays; formatting; changing pointer internal pointer casts mechanics.

--- src/avr_io/progmem.nim
+++ src/avr_io/progmem.nim
@@ -1,54 +1,77 @@
 import macros
 import strutils
 
-# TODO: atmega1284? type FarProgramMemory*[T] = distinct T
-type PmNumber = SomeUnsignedInt | float32
-type ProgramMemory*[T: PmNumber] = distinct T
-type ProgramMemoryArray*[S: static[int]; T: PmNumber] = distinct array[S, T]
+# TODOs: 
+# - [ ] support for far operations
+#   - [ ] type FarProgramMemory*[T] = distinct T
+#   - [ ] add atmega1284 support (registers/interrupts) once we're at it?
+# - [x] unify types? memcpy_P/PF may be used to solve pm[] accesses for sizes > 4B
+#   - [ ] unify progmem defs and allow 1+ defs in one block
+#   - [ ] compile time replace in progmem objects (fields -> .fields in C)
+# - [ ]wrap other _P and _PF functions in pgmspace.h
 
-template pgmPtr[T](pm: ProgramMemory[T]): uint16 =
+type ProgramMemory*[T] = distinct T
+
+template pgmPtr[T](pm: ProgramMemory[T]): ptr T =
+  unsafeAddr T(pm)
+
+template pgmPtrU16[T](pm: ProgramMemory[T]): uint16 =
   cast[uint16](unsafeAddr pm)
 
-template pgmPtr[S; T](pm: ProgramMemoryArray[S, T], offset: int): uint16 =
+template pgmPtrOffset[S; T](pm: ProgramMemory[array[S, T]]; offset: int): ptr T =
+  unsafeAddr array[S, T](pm)[offset]
+
+template pgmPtrOffsetU16[S; T](pm: ProgramMemory[array[S, T]], offset: int): uint16 =
   cast[uint16](unsafeAddr array[S, T](pm)[offset])
+
 
 proc readByteNear(a: uint16): uint8 {.importc: "pgm_read_byte", header:"<avr/pgmspace.h>".}
 proc readWordNear(a: uint16): uint16 {.importc: "pgm_read_word", header:"<avr/pgmspace.h>".}
 proc readDWordNear(a: uint16): uint32 {.importc: "pgm_read_dword", header:"<avr/pgmspace.h>".}
 proc readFloatNear(a: uint16): float32 {.importc: "pgm_read_float", header:"<avr/pgmspace.h>".}
+proc memCompare[T](s1, s2: ptr T, s: int): int {.importc: "memcmp_P", header: "<avr/pgmspace.h>".} 
+proc memCountCopy() {.importc: "memccpy_P", header: "<avr/pgmspace.h>".}
+proc memCopy[T](dest, src: ptr T; len: csize_t) {.importc: "memcpy_P", header: "<avr/pgmspace.h>".}
+proc strCat() {.importc: "strcat_P", header: "<avr/pgmspace.h>".}
+proc strCompare() {.importc: "strcmp_P", header: "<avr/pgmspace.h>".} 
+proc strCopy() {.importc: "strcpy_P", header: "<avr/pgmspace.h>".}
+proc strNCompare() {.importc: "strncmp_P", header: "<avr/pgmspace.h>".} 
+proc strNCat() {.importc: "strncat_P", header: "<avr/pgmspace.h>".}
+proc strNCopy() {.importc: "strncpy_P", header: "<avr/pgmspace.h>".}
+proc strStr() {.importc: "strstr_P", header: "<avr/pgmspace.h>".} 
 
 template `[]`*[T](pm: ProgramMemory[T]): T =
   when typeof(T) is float32:
-    readFloatNear(pgmPtr(pm))
+    readFloatNear(pgmPtrU16(pm))
+  elif sizeof(T) == 1:
+    readByteNear(pgmPtrU16(pm))
+  elif sizeof(T) == 2:
+    readWordNear(pgmPtrU16(pm))
+  elif sizeof(T) == 4:
+    readDWordNear(pgmPtrU16(pm))
   else:
-    when sizeof(T) == 1:
-      readByteNear(pgmPtr(pm))
-    elif sizeof(T) == 2:
-      readWordNear(pgmPtr(pm))
-    elif sizeof(T) == 4:
-      readDWordNear(pgmPtr(pm))
-    else:
-      static:
-         error("unsupported size for ProgramMemory.read")
+    var e: T
+    memCopy(addr e, pgmPtr(pm), csize_t(sizeof(T)))
+    e
 
-template len*[S; T](pm: ProgramMemoryArray[S, T]): untyped =
-  array[S, T](pm).len
+template len*[S; T](pm: ProgramMemory[array[S, T]]): untyped = S
 
-template `[]`*[S; T](pm: ProgramMemoryArray[S, T]; offset: int): T =
+template `[]`*[S; T](pm: ProgramMemory[array[S, T]]; offset: int): T =
   when typeof(T) is float32:
-    readFloatNear(pgmPtr(pm, offset))
+    readFloatNear(pgmPtrOffsetU16(pm, offset))
   else:
     when sizeof(T) == 1:
-      readByteNear(pgmPtr(pm, offset))
+      readByteNear(pgmPtrOffsetU16(pm, offset))
     elif sizeof(T) == 2:
-      readWordNear(pgmPtr(pm, offset))
+      readWordNear(pgmPtrOffsetU16(pm, offset))
     elif sizeof(T) == 4:
-      readDWordNear(pgmPtr(pm, offset))
+      readDWordNear(pgmPtrOffsetU16(pm, offset))
     else:
-      static:
-         error("unsupported size for ProgramMemory.read")
+      var e: T
+      memCopy(addr e, pgmPtrOffset(pm, offset), sizeof(T))
+      e
 
-iterator progmemIter*[S; T](pm: ProgramMemoryArray[S, T]): T =
+iterator progmemIter*[S: static[int]; T](pm: ProgramMemory[array[S, T]]): T =
   var i = 0
   while i < S:
     yield pm[i]
@@ -62,18 +85,22 @@ template wrapC(s: static[string] = "", equal: bool = true): static[string] =
     
 macro progmem*(n, v: untyped): untyped =
   quote do:
-    let `n` {.importc, codegenDecl: wrapC($`v`), global, noinit.}: 
+    when typeOf(`v`) is SomeNumber:
+      const s = $`v`
+    else:
+      const s = multiReplace(($`v`), ("(", "{"), (")", "}"))
+    let `n` {.importc, codegenDecl: wrapC(s), global, noinit.}: 
       ProgramMemory[`v`.typeof]
 
 macro progmemArray*(n, v: untyped): untyped =
   quote do:
-    const s = multiReplace(($`v`), ("[", "{"),("]", "}"))
-    let `n` {.importc, codegenDecl: wrapC(s), global, noinit.}: ProgramMemoryArray[`v`.len, `v`[0].typeof]
+    const s = multiReplace(($`v`), ("[", "{"), ("]", "}"))
+    let `n` {.importc, codegenDecl: wrapC(s), global, noinit.}: ProgramMemory[array[`v`.len, `v`[0].typeof]]
 
 macro progmemArray*(n: untyped; t: type; s: static[int]): untyped =
   quote do:
-    let `n` {.importc, codegenDecl: wrapC("", false), global, noinit.}: ProgramMemoryArray[`s`, `t`]
+    let `n` {.importc, codegenDecl: wrapC("", false), global, noinit.}: ProgramMemory[array[`s`, `t`]]
 
 macro progmemString*(n: untyped; val: static[string]): untyped =
   quote do:
-    let `n` {.importc, codegenDecl: wrapC("\""&`val`&"\""), global, noinit.}: ProgramMemoryArray[`val`.len, uint8]
+    let `n` {.importc, codegenDecl: wrapC("\""&`val`&"\""), global, noinit.}: ProgramMemory[array[`val`.len, uint8]]
