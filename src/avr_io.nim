ec0435b37747d1d770fafb3788c83a6d4fc57cc6
mar <gim.marcello@gmail.com>
2023-11-16 22:33:38 +0100

    added: avr_io module documentation.

--- src/avr_io.nim
+++ src/avr_io.nim
@@ -1,3 +1,26 @@
+## AVR register bindings and utilities written in nim.
+## 
+## In order to use this library, you should define a `USING_X` symbol, where 
+## X is the capitalized name of the MCU you are targetting. Register 
+## definitions are conditionally included (not imported) depending on this 
+## symbol.
+## 
+## This is also valid for constants containing port and peripheral objects, 
+## which can be optionally used to have a smoother user-experience.
+## 
+## This module also exports the following submodules:
+## - `interrupt`, containing facilities to declare ISRs and use interrupts 
+##   from user code.
+## - `progmem`, containing a set of functionalities to store code in program 
+##   memory and interact with it.
+##
+## Refer to the example applications contained within the `examples` directory 
+## for specific use cases.
+
+
+import avr_io/[interrupt, progmem]
+export interrupt, progmem
+
 when defined(USING_ATMEGA328P):
   include avr_io/private/atmega328p
 elif defined(USING_ATMEGA640):
