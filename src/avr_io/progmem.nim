8835a538caa8c97a8a09cfa32576c6c4e2f90743
mar <gim.marcello@gmail.com>
2024-03-23 10:30:56 +0100

    added: progmem macro pragma, that allows let expressions contents to be stored in program memory by dping `let a {.progmem.} = ...`; supports literal values only for now.

--- src/avr_io/progmem.nim
+++ src/avr_io/progmem.nim
@@ -3,6 +3,7 @@
 ## manipulate it.
 
 import macros
+import tables
 
 # TODOs:
 # - [ ] support for far operations
@@ -122,7 +123,7 @@ proc readFromAddress*[T](a: uint16) : T =
     e
 
 
-iterator progmemIter*[S: static[int]; T](pm: ProgramMemory[array[S, T]]): T =
+iterator progmemIter*[S: static int; T](pm: ProgramMemory[array[S, T]]): T =
   ## Iterator that can be used to safely traverse program memory arrays.
   ## Note that this must generate a copy of each element iterated, in order to 
   ## make it available to the user. 
@@ -132,7 +133,7 @@ iterator progmemIter*[S: static[int]; T](pm: ProgramMemory[array[S, T]]): T =
     inc i
 
 
-iterator progmemIter*[S: static[int]](pm: ProgramMemory[array[S, cchar]]): cchar =
+iterator progmemIter*[S: static int](pm: ProgramMemory[array[S, cchar]]): cchar =
   ## Iterator that can be used to safely traverse program memory cchar arrays.
   ## Note that this must generate a copy of each element iterated, in order to 
   ## make it available to the user. 
@@ -155,8 +156,12 @@ proc escapeStrseq(s: string): string =
   r
 
 
-template wrapC(s: static[string] = "", equal: bool = true): static[string] =
-  when equal:
+proc wrapC(s: string = "", equal: bool = true, is_str: bool = false): string =
+  var s = s
+  if is_str:
+    s = "\"" & s & "\""
+
+  if equal:
     "static const $# $# __attribute__((__progmem__)) = " & escapeStrseq(s)
   else:
     "static const $# $# __attribute__((__progmem__))"
@@ -166,6 +171,7 @@ proc substStructFields(s: string): string =
   # Hand-rolled FSM-based struct parsing proc, since it is not possible to 
   # use the 're' module at compile-time. TODO modify to accept objects as 
   # params to objects - this may requires some parsing action? 
+  # TODO if an open ( is met, go back to identifier and call recursively
   type
     stateEnum = enum
       spaceParsing
@@ -228,6 +234,143 @@ template substBraces(s: static[string]): string =
   r
 
 
+proc eval(n: NimNode): (string, NimNode)
+
+
+proc eval_obj(nc: NimNode): (string, NimNode) =
+  var s = "{"
+  if nc.kind != nnkObjConstr:
+    error "Expected object construction, got: " & $nc.kind
+
+  for i in 1 ..< nc.len:
+    let 
+      nameNode = nc[i][0]
+      valNode  = nc[i][1]
+      (value, _) = eval(valNode)
+
+    s.add("." & nameNode.strVal() & "=" & value)
+    if i != nc.len - 1:
+      s.add(", ")
+  s.add("}")
+  (s, nc[0])
+
+
+proc eval_array(an: NimNode): (string, NimNode) =
+  var s = "{"
+  if an.kind != nnkBracket:
+    error "Expected array literal, got: " & $an.kind
+
+  var node_type: NimNode = nil
+
+  for i in 0 ..< an.len:
+    let valNode = an[i]
+    let (value, node_type_eval) = eval(valNode)
+
+    if node_type == nil:
+      node_type = node_type_eval
+
+    s.add(value)
+    if i != an.len - 1:
+      s.add(", ")
+  
+  s.add("}")
+  (s, newNimNode(nnkBracketExpr).add(
+      newIdentNode("array"),
+      newLit(an.len-1),
+      node_type
+  ))
+
+
+const
+  nnkTable = {
+    nnkCharLit:      "char",
+    nnkIntLit:       "int",
+    nnkInt8Lit:      "int8",
+    nnkInt16Lit:     "int16",
+    nnkInt32Lit:     "int32",
+    nnkInt64Lit:     "int64",
+    nnkUIntLit:      "uint",
+    nnkUInt8Lit:     "uint8",
+    nnkUInt16Lit:    "uint16",
+    nnkUInt32Lit:    "uint32",
+    nnkUInt64Lit:    "uint64",
+    nnkFloatLit:     "float",
+    nnkFloat32Lit:   "float32", 
+    nnkFloat64Lit:   "float64",
+    nnkStrLit:       "string", 
+    nnkRStrLit:      "string",
+    nnkTripleStrLit: "string"
+  }.toTable
+
+
+proc eval(n: NimNode): (string, NimNode) {.compileTime.} =
+  result = case n.kind
+  of nnkCharLit..nnkUInt64Lit:   ($n.intVal, newIdentNode(nnkTable[n.kind]))
+  of nnkFloatLit..nnkFloat64Lit: ($n.floatVal, newIdentNode(nnkTable[n.kind]))
+  of nnkStrLit..nnkTripleStrLit: ("\"" & n.strVal & "\"", newIdentNode(nnkTable[n.kind]))
+  of nnkObjConstr: eval_obj(n)
+  of nnkBracket: eval_array(n)
+  of nnkIdent: 
+    if n.strVal != "true" and n.strVal != "false":
+      raise newException(
+        ValueError, 
+        "Invalid value for eval: " & $n.kind & " (" & $n.strVal & ")")
+    (n.strVal, newIdentNode("bool"))
+  else:
+    raise newException(ValueError, "Invalid value for eval: " & $n.kind)
+
+
+macro progmem*(l: untyped): untyped =
+  ## Stores the value contained in the let expression tagged with this macro 
+  ## pragma in program memory. Use only with literals.
+  var 
+    orig = l
+    lnode = if l.kind == nnkLetSection: 
+        l[0] 
+      else: 
+        orig
+  
+  expectKind(lnode, nnkIdentDefs)
+  expectKind(lnode[0], nnkIdent)
+
+  let 
+    rval = lnode[2]
+    (str_val, node_type) = eval(rval)
+    is_string = node_type.kind == nnkIdent and node_type.strVal == "string"
+    wrapped = wrapC(str_val, true, is_string)
+  
+
+  var p = newNimNode(nnkPragmaExpr).add(
+    copyNimNode(lnode[0]),
+    newNimNode(nnkPragma).add(
+      newIdentNode("importc"),
+      newIdentNode("global"),
+      newIdentNode("noinit"),
+      newNimNode(nnkExprColonExpr).add(
+        newIdentNode("codegenDecl"),
+        newLit(wrapped)
+      )
+    )
+  )
+  lnode[0] = p
+
+  if lnode[1].kind == nnkEmpty:
+    # no type provided 
+    lnode[1] = newNimNode(nnkBracketExpr).add(
+      newIdentNode("ProgramMemory"),
+      node_type
+    )
+  else:
+    # TODO: check generics and do brackets trick
+    lnode[1] = newNimNode(nnkBracketExpr).add(
+      newIdentNode("ProgramMemory"),
+      lnode[1]
+    )
+
+  lnode[2] = newNimNode(nnkEmpty)  
+  orig
+
+
 macro progmem*(n, v: untyped): untyped =
   ## Stores the value `v` in program memory, and creates a new symbol `n` 
   ## through which it is possible to access it. 
@@ -237,7 +380,7 @@ macro progmem*(n, v: untyped): untyped =
       let `n` {.importc, codegenDecl: wrapC(s), global, noinit.}: 
         ProgramMemory[`v`.typeof]
     elif typeof(`v`) is string:
-      let `n` {.importc, codegenDecl: wrapC("\""&`v`&"\""), global, noinit.}: 
+      let `n` {.importc, codegenDecl: wrapC(`v`, true, true), global, noinit.}: 
         ProgramMemory[array[`v`.len + 1, cchar]]
     else:
       const s = substStructFields(($`v`))
