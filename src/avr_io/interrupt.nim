6c99d2c5749a306a92fded792ef93aefc84552c4
mar <gim.marcello@gmail.com>
2024-10-30 08:14:50 +0100

    added: isr can now be also called with a flag specifying various wrapping details for the ISR that gets generated by the compiler.

--- src/avr_io/interrupt.nim
+++ src/avr_io/interrupt.nim
@@ -26,19 +26,25 @@ else:
     error "undefined architecture"
 
 
-# TODO: IST attributes (block, non block, naked, aliasof)
-template vectorDecl(n: int): string =
-  "$1  __vector_" & $n & 
-  """
-    $3 __attribute__((__signal__,__used__,__externally_visible__)); 
-    $1 __vector_""" & $n & "$3"
+type
+  IsrFlag* = enum
+    IsrBlock   = ""
+    IsrNoBlock = "__attribute__((__interrupt__))"
+    IsrNaked   = "__attribute__((__naked__))"
+  IsrFlags* = set[IsrFlag]
 
 
-macro isr*(v: static[VectorInterrupt], p: untyped): untyped =
-  ## Turns the passed procedure into an interrupt service routine.
-  ## This macro applies a series of pragmas to the procedure, that are
-  ## necessary to map it to the specified interrupt handle.
-  ## Use as a macro pragma.
+template vectorDecl(n: int, flags: IsrFlags): string =
+  var attrs = ""
+  for flag in IsrFlag.items():
+    if flag in flags:
+      attrs &= " " & $flag
+  "$1  __vector_" & $n &
+  "$3 __attribute__((__signal__,__used__,__externally_visible__)); " &
+   attrs & " $1 __vector_" & $n & "$3"
+
+
+macro isrIm(v: static VectorInterrupt, f: static IsrFlags, p: typed): untyped =
   var pnode = p
   if p.kind == nnkStmtList:
     pnode = p[0]
@@ -47,15 +53,41 @@ macro isr*(v: static[VectorInterrupt], p: untyped): untyped =
   for node in pnode:
     if node.kind == nnkFormalParams:
       if node.len != 1 or node[0].kind != nnkEmpty:
-          error "an ISR must have the following signature `proc f()`"
+        error "an ISR must have the following signature `proc f()`"
 
   addPragma(pnode, newIdentNode("exportc"))
-  addPragma(pnode, 
+  addPragma(pnode,
     newNimNode(nnkExprColonExpr).add(
-      newIdentNode("codegenDecl"), newLit(vectorDecl(ord(v)))
+      newIdentNode("codegenDecl"), newLit(vectorDecl(ord(v), f))
     )
   )
   pnode
+
+
+template isr*(v: VectorInterrupt, p: typed): untyped =
+  ## Turns the passed procedure into an interrupt service routine.
+  ## This macro applies a series of pragmas to the procedure, that are
+  ## necessary to map it to the specified interrupt handle.
+  ## Use as a macro pragma.
+  isrIm(v, {}, p)
+
+
+template isr*(v: VectorInterrupt, f: IsrFlags, p: typed): untyped =
+  ## Turns the passed procedure into an interrupt service routine, with the
+  ## specified ISR flags to alter its behaviour:
+  ##   - IsrBlock: default behaviour, interrupts will be disabled for the
+  ##   duration of the ISR.
+  ##   - IsrNoBlock: interrupts will still be enabled during the ISR handling;
+  ##   note that this adds minimal overhead; use when in need of nested
+  ##   interrupts.
+  ##   - IsrNaked: no preamble or epilogue will be generated for the ISR.
+  ##   This means that the registers and return instruction must be manually
+  ##   handled.
+  ## This macro applies a series of pragmas to the procedure, that are
+  ## necessary to map it to the specified interrupt handle.
+  ## Use as a macro pragma.
+  isrIm(v, f, p)
+
 
 template sei*() =
   ## Sets the global interrupt flag within the status register, 
@@ -66,6 +98,7 @@ template sei*() =
     :
     : "memory"
   """
+
 
 template cli*() =
   ## Clears the global interrupt flag within the status register, 
